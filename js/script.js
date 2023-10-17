e26f3c245874e761571db206ee38b08d34479f2b
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-10-17 15:42:48 +0200

    add: raccolta dati, logica

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Raccolta dati
+
+//Chiedo nome
+const username = prompt("Benvenuto utente! Qual'è il tuo nome?");
+console.log(username, typeof username);
+//Chiedo cognome
+const secondname = prompt(
+  "Ottimo! ora inserisci il tuo cognome per proseguire"
+);
+console.log(secondname, typeof secondname);
+//Chiedo colore preferito
+const favColor = prompt(
+  "Adesso inserisci il tuo colore preferito e otterrai una password sicurissima!"
+);
+console.log(favColor, typeof favColor);
+
+//Logica del programma
+
+const password = `${username}${secondname}${favColor}21`;
+console.log(password);
